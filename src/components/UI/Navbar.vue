9bbc857852746804d12dee16ebab1ad4417a3b19
GINSENG-A7 <zhenya_bykov_1998@mail.ru>
2022-07-17 04:35:46 +0300

    routing

---
+++ src/components/UI/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="navbar">
+		<div class="navbar__logo" @click="this.$router.push({ path: '/' })">Vue 3</div>
+		<div class="navbar__btns">
+			<my-button @click="this.$router.push('/posts')">Посты</my-button>
+			<my-button @click="this.$router.push('/about')">О сайте</my-button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "navbar",
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+	height: 50px;
+	margin: 0 auto;
+	padding: 0 2.5%;
+	width: 100%;
+	background-color: teal;
+	box-shadow: 2px 2px 4px gray;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	&__logo {
+		color: #FFF;
+		width: max-content;
+		font-size: 40px;
+	}
+	&__btns {
+		.btn {
+			margin: 0 5px;
+		}
+	}
+}
+</style>
---
+++ src/pages/UserPage.vue
@@ -0,0 +1,168 @@
+<template>
+	<div class="app container">
+		<h1>Страница с постами</h1>
+		<my-input placeholder="Поиск..." v-model="searchQuery"></my-input>
+		<div class="container__btns">
+			<my-button @click="showDialog">Создать пользователя</my-button>
+			<my-select v-model="selectedSort" :options="sortOptions"/>
+		</div>
+		<my-dialog v-model:show="dialogVisible">			
+			<post-form @create="createPost" style="margin-top: 30px"/>
+		</my-dialog>
+		<post-list :posts="sortedAndSearchedPosts" @remove="removePost" v-if="!isPostsLoading" style="margin-top: 20px"/>
+		<div class="loading" v-else>Посты загружаются...</div>
+		<!-- <div class="page__wrapper">
+			<div class="page" :class="{'current-page': page == pageNumber}" v-for="pageNumber in totalPages" :key="pageNumber" @click="changePage(pageNumber)">{{ pageNumber }}</div>
+		</div> -->
+		<div ref="observer" class="observer"></div>
+	</div>
+</template>
+
+<script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+import axios from 'axios';
+import MyInput from "@/components/UI/MyInput.vue";
+export default {
+	components: {
+    PostList,
+    PostForm,
+    MyInput
+},
+	data() {
+		return {
+			posts: [],
+			dialogVisible: false,
+			isPostsLoading: false,
+			selectedSort: "",
+			searchQuery: "",
+			page: 1,
+			limit: 10,
+			totalPages: 0,
+			lastLoadedCount: 0,
+			sortOptions: [
+				{value: "title", name: "По названию"},
+				{value: "body", name: "По содержимому"},
+			],
+		}
+	},
+	methods: {
+		createPost(post) {
+			this.posts.push(post);
+		},
+		removePost(post) {
+			this.posts = this.posts.filter(p => p.id !== post.id)
+		},
+		showDialog() {
+			this.dialogVisible = true;
+		},
+		// changePage(pageNumber) {
+		// 	this.page = pageNumber;
+		// },
+		async fetchPosts() {
+			try {
+				this.isPostsLoading = true;
+				const response = await axios.get("https://jsonplaceholder.typicode.com/posts", {
+					params: {
+						_page: this.page,
+						_limit: this.limit,
+					} 
+				});
+				this.totalPages = Math.ceil(response.data.length / this.limit);
+				this.posts = response.data;
+				this.lastLoadedCount = response.data.length;
+			}
+			catch(e) {
+				alert("Ошибка сервера");
+			}
+			finally {
+				this.isPostsLoading = false;
+			}
+		},
+		async loadMorePosts() {
+			try {
+				this.page += 1;
+				// this.isPostsLoading = true;
+				const response = await axios.get("https://jsonplaceholder.typicode.com/posts", {
+					params: {
+						_page: this.page,
+						_limit: this.limit,
+					} 
+				});
+				this.totalPages = Math.ceil(response.data.length / this.limit);
+				this.posts = [...this.posts, ...response.data];
+				this.lastLoadedCount = response.data.length;
+			}
+			catch(e) {
+				alert("Ошибка сервера");
+			}
+		}
+	},
+	mounted() {
+		this.fetchPosts();
+		const options = {
+			rootMargin: "0px",
+			threshold: 1.0,
+		};
+		const callback = (entries, observer) => {
+			console.log(this.lastLoadedCount);
+			if (entries[0].isIntersecting && this.lastLoadedCount === this.limit) {
+				this.loadMorePosts();
+			}
+		};
+		const observer = new IntersectionObserver(callback, options);
+		observer.observe(this.$refs.observer);
+	},
+
+	// watch: {
+	// 	selectedSort(newValue) {
+	// 		this.posts.sort((postA, postB) => {
+	// 			return postA[newValue]?.localeCompare(postB[newValue]);
+	// 		});
+	// 	},
+	// },
+
+	// watch: {
+	// 	page() {
+	// 		this.fetchPosts();
+	// 	}
+	// },
+	computed: {
+		sortedPosts() {
+			return [...this.posts].sort((postA, postB) => {
+				return postA[this.selectedSort]?.localeCompare(postB[this.selectedSort]);
+			});
+		},
+		sortedAndSearchedPosts() {
+			return this.sortedPosts.filter(({title, body}) => {
+				return (title + " " + body).toLowerCase().includes(this.searchQuery.toLowerCase());
+			});
+		}
+	},
+}
+</script>
+
+<style lang="scss">
+	.container__btns {
+		display: flex;
+		justify-content: space-between;
+	}
+	.page__wrapper {
+		display: flex;
+		margin-top: 15px;
+		margin-bottom: 10px;
+		.page {
+			border: 2px solid black;
+			padding: 6px;
+			background-color:gainsboro;
+		}
+		.current-page {
+			background-color: #FFF;
+			border: 2px solid teal;
+		}
+	}
+	.observer {
+		height: 30px;
+		background-color: greenyellow;
+	}
+</style>
